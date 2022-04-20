7f92d577eef0d5c9122d18739e313c72c1c8a765
FrancoRodriguez99 <juanfrancorodriguez99@gmail.com>
2022-04-20 11:17:41 -0300

    more beauty

--- src/components/newQuote.js
+++ src/components/newQuote.js
@@ -1,44 +1,53 @@
-import { toBeInTheDocument, toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import quotes from "./quotes";
-import { useSelector } from "react-redux";
 
 class NewQuote extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        var random = Math.floor(Math.random() * quotes.length);
-        
-        this.state = {
-            quote: quotes[random].quote,
-            author: quotes[random].author
-        };
-        this.changeQuote = this.changeQuote.bind(this)
-        
-    }
+    var random = Math.floor(Math.random() * quotes.length);
 
-    changeQuote() {
-        var random = Math.floor(Math.random() * quotes.length);
-        this.setState({
-            quote: quotes[random].quote,
-            author: quotes[random].author
-        })
-    }
+    this.state = {
+      quote: quotes[random].quote,
+      author: quotes[random].author,
+    };
+    this.changeQuote = this.changeQuote.bind(this);
+  }
 
-    render() {
-        return(
-        <div id="quote-box">
-            <div id="quote">
-            <p id="text">{this.state.quote}</p>
-            <p id="author"> {this.state.author}</p>
-            </div>
-            <div id="options">
-                <button id="new-quote" class="btn btn-primary" onClick={this.changeQuote}>New Quote <i class="fa fa-quote-left"> </i></button>
-                <a className="twitter-share-button" id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${this.state.quote}--${this.state.author}`}>Tweet this one! <i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </div>
-        </div>)
-    }
-};
+  changeQuote() {
+    var random = Math.floor(Math.random() * quotes.length);
+    this.setState({
+      quote: quotes[random].quote,
+      author: quotes[random].author,
+    });
+  }
 
+  render() {
+    return (
+      <div id="quote-box" className="button-85">
+        <div id="quote">
+          <p id="text">{this.state.quote}</p>
+          <p id="author"> {this.state.author}</p>
+        </div>
+        <div id="options">
+          <button
+            id="new-quote"
+            class="btn btn-primary"
+            onClick={this.changeQuote}
+          >
+            New Quote <i class="fa fa-quote-left"> </i>
+          </button>
+          <a
+            className="twitter-share-button"
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${this.state.quote}--${this.state.author}`}
+          >
+            Tweet this one! <i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
-export default NewQuote
+export default NewQuote;
